4ca76563da517bd11388057a9b77b847d04a8e76
neharajesh <leehanseok3@gmail.com>
2020-02-13 13:24:48 +0530

    minor changes

--- src/main/java/mainapp/NehsPizzaApplication.java
+++ src/main/java/mainapp/NehsPizzaApplication.java
@@ -1,10 +1,12 @@
+package mainapp;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NehsPizzaApplication {
-	
-	public static void main(String... args) {
+	public static void main(String[] args) {
 		SpringApplication.run(NehsPizzaApplication.class, args);
 	}
+
 }
